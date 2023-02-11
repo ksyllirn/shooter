9997db121cffba36ab5f876e6014b923fe9fa3b6
Ксения Куликовских <115486017+ksyllirn@users.noreply.github.com>
2023-02-11 19:16:42 +0500

    Create pre_main.py

---
+++ pre_main.py
@@ -0,0 +1,96 @@
+from pygame import *
+from random import randint
+
+
+
+window_width = 700
+window_height = 500
+
+window = display.set_mode((window_width,window_height))
+display.set_caption("shooter")
+background = transform.scale(image.load("back.jpg"), (window_width,window_height))
+
+count = 0
+missed = 0
+#mixer.init()
+#mixer.music.load("back_music.ogg")
+#mixer.music.play()
+#fire_sound = mixer.Sound("")
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (65,65))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image,(self.rect.x,self.rect.y))
+
+
+class Player(GameSprite):
+    def update(self):
+        keys_pressed = key.get_pressed()
+
+        if keys_pressed[K_a] and self.rect.x >= 10:
+            self.rect.x -= self.speed
+
+        if keys_pressed[K_d] and self.rect.x <= 625:
+            self.rect.x += self.speed
+
+        if keys_pressed[K_SPACE]:
+            self.fire()
+    
+    def fire(self):
+        pass
+        #fire_sound.play()
+
+
+class Enemy(GameSprite):
+    def update(self):
+        global missed
+        self.rect.y += self.speed
+        if self.rect.y >= 500:
+            self.rect.y = 0
+            missed += 1
+
+
+hero = Player("hero.png", window_width//2 , 400, 6)
+
+enemies = sprite.Group()
+for i in range(5):
+    enemies.add(Enemy("enemy.png", randint(10,625),10, 1))
+
+clock = time.Clock()
+game = True
+finish = False
+FPS = 60
+
+font.init()
+
+
+while game:
+    if not finish:        
+        window.blit(background, (0,0))
+
+        hero.update()
+        hero.reset()
+
+        enemies.update()
+        enemies.draw(window)
+
+        count_text = font.Font(None, 40).render(f"count: {count}", True, (224,224,224))
+        missed_text = font.Font(None, 40).render(f"missed: {missed}", True, (224,224,224))
+
+        window.blit(count_text,(10,10))
+        window.blit(missed_text,(10,35))
+
+
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+    display.update()
+    clock.tick(FPS)
